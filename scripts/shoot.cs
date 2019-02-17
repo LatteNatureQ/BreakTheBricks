88c90dd7d523294953283f353d2ade392d03f4a5
拿铁自然卷 <2895358020@qq.com>
2019-02-18 01:25:25 +0800

    commit

---
+++ scripts/shoot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class shoot : MonoBehaviour {
+    
+    public GameObject build;
+    public float speed = 5;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("start ok");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //判断鼠标左键是否按下
+        if (Input.GetMouseButtonDown(0))
+        {
+            //实例化小球
+            GameObject b = GameObject.Instantiate(build, transform.position, transform.rotation);
+            Rigidbody rgb = b.GetComponent<Rigidbody>();
+            rgb.velocity = transform.forward * speed;
+        }
+        Debug.Log("Update ok");
+	}
+}
